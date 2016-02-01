069481ccfa1ed23a0bd7d8304935c06ac7238b34
IrinaBorisenko <aurorasray@gmail.com>
2016-02-01 18:48:58 +0200

    Refactor and Realization without states in onNotify

--- src/com/example/Observer/ObserverPatternWithoutStates.java
+++ src/com/example/Observer/ObserverPatternWithoutStates.java
@@ -1,4 +1,4 @@
-package com.example.observer;
+package com.example.Observer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +7,7 @@ import java.util.List;
  * Created by: Iryna Borysenko
  * Date: 2/1/16
  */
-public class observer {
+public class ObserverPatternWithoutStates {
 
     public static void main(String[] args) {
 
@@ -17,55 +17,42 @@ public class observer {
         baby.attachObserver(new Dog());
         baby.attachObserver(new DogMaster());
 
-        baby.babyPlay();
-        baby.babyDog();
-        baby.babyFall();
-        baby.babyCry();
-
+        System.out.println("Baby play in sandbox.");
+        baby.setState(State.STATE_BABY_PLAY);
+        System.out.println("Baby noticed the dog. Baby does to a dog.");
+        baby.setState(State.STATE_BABY_GO);
+        System.out.println("Baby fell.");
+        baby.setState(State.STATE_BABY_FALL);
+        System.out.println("Baby starts to cry.");
+        baby.setState(State.STATE_BABY_CRY);
     }
 
     interface Observable {
         void attachObserver(Observer observer);
         void detachObsever(Observer observer);
-        void notifyObservers(State state);
+        void notifyObservers();
     }
 
     interface Observer {
-        void onNotify(State state);
+        void onNotify();
     }
 
     static class Baby implements Observable {
 
-        private List<Observer> mObservers = new ArrayList<Observer>();
-        private State mState;
+        final private List<Observer> mObservers = new ArrayList<Observer>();
 
-        void babyPlay() {
-            System.out.println("Baby play in sandbox.");
-            mState = State.BABY_PLAY;
-            notifyObservers(mState);
+        private static State mState;
+
+
+        public static State getState() {
+            return mState;
         }
 
-        void babyDog() {
-            System.out.println("Baby noticed the dog. Baby does to a dog.");
-            mState = State.GO_DOG;
-            notifyObservers(mState);
+        public void setState(State mState) {
+            Baby.mState = mState;
+            notifyObservers();
         }
 
-        void babyFall() {
-            System.out.println("Baby fell.");
-            mState = State.BABY_FALL;
-            notifyObservers(mState);
-        }
-
-        void babyCry()  {
-            System.out.println("Baby starts to cry.");
-            mState = State.BABY_CRY;
-            notifyObservers(mState);
-        }
-
-        /**
-         * I don't understand meaning of this strings
-         */
         @Override
         public void attachObserver(Observer observer) {
             mObservers.add(observer);
@@ -77,9 +64,9 @@ public class observer {
         }
 
         @Override
-        public void notifyObservers(State state) {
+        public void notifyObservers() {
             for(Observer o : mObservers) {
-                o.onNotify(state);
+                o.onNotify();
             }
         }
     }
@@ -87,19 +74,19 @@ public class observer {
     static class Mother implements Observer {
 
         @Override
-        public void onNotify(State state) {
+        public void onNotify() {
 
-            switch (state) {
-                case BABY_PLAY:
+            switch (Baby.getState()) {
+                case STATE_BABY_PLAY:
                     System.out.println("Mother talks to the other mothers.");
                     break;
-                case GO_DOG:
+                case STATE_BABY_GO:
                     System.out.println("Mother watching a baby.");
                     break;
-                case BABY_FALL:
+                case STATE_BABY_FALL:
                     System.out.println("Mother runs to a baby.");
                     break;
-                case BABY_CRY:
+                case STATE_BABY_CRY:
                     System.out.println("Mother seeking baby injury.");
                     break;
                 default:
@@ -111,19 +98,19 @@ public class observer {
     static class Dog implements Observer {
 
         @Override
-        public void onNotify(State state) {
+        public void onNotify() {
 
-            switch (state) {
-                case BABY_PLAY:
+            switch (Baby.getState()) {
+                case STATE_BABY_PLAY:
                     System.out.println("Dog is looking for a bone.");
                     break;
-                case GO_DOG:
+                case STATE_BABY_GO:
                     System.out.println("Dog begins to run.");
                     break;
-                case BABY_FALL:
+                case STATE_BABY_FALL:
                     System.out.println("Dog stopped.");
                     break;
-                case BABY_CRY:
+                case STATE_BABY_CRY:
                     System.out.println("Dog goes away.");
                     break;
                 default:
@@ -135,16 +122,16 @@ public class observer {
     static class DogMaster implements Observer {
 
         @Override
-        public void onNotify(State state) {
+        public void onNotify() {
 
-            switch (state) {
-                case BABY_PLAY:
+            switch (Baby.getState()) {
+                case STATE_BABY_PLAY:
                     System.out.println("Dog master is on the phone.");
                     break;
-                case GO_DOG:
+                case STATE_BABY_GO:
                     System.out.println("Dog master follow dog.");
                     break;
-                case BABY_CRY:
+                case STATE_BABY_CRY:
                     System.out.println("Dog master doesn't hear voice from phone because of yell.");
                     break;
                 default:
@@ -153,7 +140,7 @@ public class observer {
         }
     }
     enum State {
-        BABY_PLAY, GO_DOG, BABY_FALL, BABY_CRY
+        STATE_BABY_PLAY, STATE_BABY_GO, STATE_BABY_FALL, STATE_BABY_CRY
     }
 
 }
